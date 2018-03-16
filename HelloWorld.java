fe9158878e095150820fcf9d6db764a342801687
Geng-WD <1204132264@qq.com>
2018-03-16 13:19:48 +0800

    Hello Git

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld{
+//这是main方法，程序入口
+ public static void main(String[] args){
+   /*
+  输出语句 System.out.println();
+  */
+      System.out.println("Hello Java!");
+      System.out.println("Hello Git!");
+
+  }
+}
